b22cc10b3d81676764029668309866b661abbd7a
Valtteri Juvonen <valtteri.juvone@hotmail.com>
2023-08-10 16:31:08 +0300

    More ui

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -2,7 +2,13 @@ import { Box, Heading, Section, Text } from "@radix-ui/themes";
 import Image from "next/image";
 import heroImage from "../../public/hero.jpg";
 import Link from "next/link";
-
+import location from "../../public/location.svg";
+import phone from "../../public/phone.svg";
+import mail from "../../public/mail.svg";
+import fb from "../../public/fb.svg"
+import youtube from "../../public/youtube.svg"
+import gram from "../../public/gram.svg"
+import whatsapp from "../../public/whatsapp.svg"
 
 export default function Footer() {
     
@@ -11,27 +17,38 @@ export default function Footer() {
             <Box className=" flex flex-row border-b-gray-500 pb-4 border-b">
                 <Box className=" w-1/3 flex flex-col">
                     <Heading size="6" as="h2" className=" pb-4">Contact</Heading>
-                    <Text className=" text-xs">Calle jecinto Benavente</Text>
-                    <Text className=" text-xs">29601 Marbella</Text>
-                    <Text className=" text-xs">Malaga</Text>
+                    <Box className=" flex flex-row items-start">
+                        <Image width={8} src={location} alt="location" className=" pt-1" />
+                        <Text className=" text-xs pl-1">
+                            Calle Jacinto Benavente, Nº 429640 Fuengirola (Málaga)
+                        </Text>
+                    </Box>
+                    <Box className=" flex flex-row">
+                        <Image width={10} src={phone} alt="phone" />
+                        <Text className=" text-xs pl-1">+34  694 413 269</Text>
+                    </Box>
+                    <Box className=" flex flex-row">
+                        <Image width={14} src={mail} alt="email" />
+                        <Text className=" text-xs pl-1">solexhomes@gmail.com</Text>
+                    </Box>
                 </Box>
                 <Image className=" w-1/3" src={heroImage} alt="logo" />
                 <Box className=" w-1/3 flex flex-col items-end">
                     <Heading size="6" as="h2" className=" pb-4">FOLLOW US</Heading>
-                    <Box className=" flex flex-col">
-                        <Text className=" text-xs">X</Text>
-                        <Text className=" text-xs">X</Text>
-                        <Text className=" text-xs">X</Text>
-                        <Text className=" text-xs">X</Text>
+                    <Box className=" flex flex-row gap-3">
+                        <Image height={24} src={whatsapp} alt="whatsapp" />
+                        <Image height={24} src={fb} alt="facebook" />
+                        <Image height={24} src={gram} alt="instagram" />
+                        <Image height={24} src={youtube} alt="youtube" />
                     </Box>
                 </Box>
             </Box>
             <Box className=" flex flex-row justify-between gap-4 pt-4">
                 <Text className=" text-xs">Copyright @ 2023. All rights reserved.</Text>
-                <Box className="flex flex-row gap-2 text-xs">
+                <Box className="flex flex-row gap-1 text-xs text-orange-300 underline">
                     <Link href="/legal">Legal Info</Link>
-                    <Link href="/Privacy">Privacy Policy</Link>
-                    <Link href="/cookies">Cookies Policy</Link>
+                    <Link href="/Privacy" className=" border-l pl-1 border-black">Privacy Policy</Link>
+                    <Link href="/cookies" className=" border-l pl-1 border-black">Cookies Policy</Link>
                 </Box>
             </Box>
         </Section>
--- components/Header/TopHeader.tsx
+++ components/Header/TopHeader.tsx
@@ -1,18 +1,31 @@
+import { Box } from "@radix-ui/themes"
+import Image from "next/image"
+import phone from "../../public/phone.svg"
+import mail from "../../public/mail.svg"
+import youtube from "../../public/youtube.svg"
+import gram from "../../public/gram.svg"
+import fb from "../../public/fb.svg"
+import whatsapp from "../../public/whatsapp.svg"
 
 export default function TopHeader() {
     return (
         <div className=" text-red-200 bg-zinc-50 h-10 w-full absolute top-0 left-0">
             <div className=" header w-full h-full flex flex-row justify-between align-middle pr-5 pl-5">
-                <div className=" flex flex-row gap-4 items-center justify-center text-xs text-black">
-                    <p>+34 694 413 269</p>
-                    <p>solexhomes@gmail.com</p>
+                <div className=" flex flex-row gap-2 items-center justify-center text-xs text-black">
+                    <Box className=" flex flex-row items-center gap-1">
+                        <Image height={10} src={phone} alt="phone" />
+                        <p>+34 694 413 269</p>
+                    </Box>
+                    <Box className=" border-l border-black pl-2 flex flex-row items-center gap-1">
+                        <Image height={16} src={mail} alt="mail" />
+                        <p>solexhomes@gmail.com</p>
+                    </Box>
                 </div>
-                <div className=" flex flex-row items-center gap-2 text-xs text-black">
-                    <p>X</p>
-                    <p>X</p>
-                    <p>X</p>
-                    <p>X</p>
-                    <p>X</p>
+                <div className=" gap-5 flex flex-row items-center gap-2 text-xs text-black">
+                    <Image height={18} src={whatsapp} alt="whatsapp" />
+                    <Image height={18} src={fb} alt="facebook" />
+                    <Image height={18} src={gram} alt="instagram" />
+                    <Image height={18} src={youtube} alt="youtube" />
                 </div>
             </div>
         </div>
